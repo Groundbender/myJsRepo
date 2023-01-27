f72b3c56b9bcaa50f34b2ff660b419a3fd28b3c4
Damir <damir24may@mail.ru>
2023-01-27 15:09:02 +0300

    added lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "калькулятор";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 30;
+let rollback = 10;
+let fullPrice = 100;
+let adaptive = false;
 
-alert("Hello World!");
-console.log("Hello World!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(screenPrice + " долларов");
+console.log(fullPrice + " долларов");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(
+  "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
+);
