b376b4e7b983f8535fd903ef6c56b0822df10c9a
Damir <damir24may@mail.ru>
2023-01-30 19:05:10 +0300

    almost lesso03

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let title = "калькулятор";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 30;
+"use strict";
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt(
+  'Какие типы экранов нужно разработать? (пример: "Простые, Сложные, Интерактивные")'
+);
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+
+let adaptive = Boolean(prompt("Нужен ли адаптив на сайте?"));
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 10;
 let fullPrice = 100;
-let adaptive = false;
+
+let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+
+console.log(servicePercentPrice);
+
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice <= 30000 && fullPrice > 15000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice <= 15000 && fullPrice > 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
