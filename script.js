1cac9cd4c3fe29b19281cf2ab7d2c1e3fb2e0a49
Damir <damir24may@mail.ru>
2023-02-05 18:32:36 +0300

    lesson04

--- script.js
+++ script.js
@@ -13,32 +13,54 @@ let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+let fullPrice;
+let servicePercentPrice;
+let allServicePrices;
 
-console.log(servicePercentPrice);
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
 
-if (fullPrice > 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice <= 30000 && fullPrice > 15000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice <= 15000 && fullPrice > 0) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что-то пошло не так");
+const getRollbackMessage = function (price) {
+  if (price > 30000) {
+    return "Даем скидку в 10%";
+  } else if (price <= 30000 && price > 15000) {
+    return "Даем скидку в 5%";
+  } else if (price <= 15000 && price > 0) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что-то пошло не так";
+  }
+};
+
+const getAllServicePrices = function (add1, add2) {
+  return add1 + add2;
+};
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
 }
+fullPrice = getFullPrice();
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+function getTitle(str) {
+  let text = str.trim();
+  let result = text.slice(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+  return result;
+}
+title = getTitle(title);
 
-console.log(screens.length);
+function getServicePercentPrices() {
+  return fullPrice - fullPrice * (rollback / 100);
+}
+servicePercentPrice = getServicePercentPrices();
 
-console.log(screenPrice + " долларов");
-console.log(fullPrice + " долларов");
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 
 console.log(screens.toLowerCase().split(", "));
 
-console.log(
-  "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
-);
+console.log(getServicePercentPrices());
